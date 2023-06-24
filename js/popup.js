2e104e0325f6c863e2d76824f9ee64f2ef6f047f
GDprints <120603682+GDprint@users.noreply.github.com>
2023-06-24 22:46:47 +0400

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+function togglePopup1(){
+    document.getElementById("popup-1").classList.toggle("active");
+    
+}
+
+function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+    
+}                                                                          S
