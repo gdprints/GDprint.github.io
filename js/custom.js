450c85d16fa72864dc59a57a48bb0bf83926a812
GDprints <120603682+GDprint@users.noreply.github.com>
2024-01-07 00:18:24 +0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function ($) {
   (function () {
 
     var $menu = $('.navigation nav'),
-      optionsList = '<option value="" selected>Go to..</option>';
+      optionsList = '<option value="" selected>Մենյու..</option>';
 
     $menu.find('li').each(function () {
         var $this = $(this),
