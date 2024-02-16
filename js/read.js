c26786d1fb08db9eeeeba2def61d0abf03d62c5d
GDprints <120603682+GDprint@users.noreply.github.com>
2024-02-16 15:53:04 +0400

    Update read.js

--- js/read.js
+++ js/read.js
@@ -1,9 +1,20 @@
+// form repeater
 $(document).ready(function(){
-    $(".invisible-content").hide();
-    $(document).on('click',"#btn",function(){
-        var moreLessButton=$(".invisible-content").is(":visible")?'Կարդալ ավելին':'Ցույցադրել քիչ';
-        $(this).text(moreLessButton);
-        $(this).parent(".span8").find(".invisible-content").toggle();
-        $(this).parent(".span8").find(".visible-content").toggle();
-    });
-});
+    $('.repeater').repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': ''
+        },
+        show:function(){
+            $(this).slideDown();
+        },
+        hide: function(deleteElement){
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                generateCV();
+            }, 500);
+        },
+        isFirstItemUndeletable: true
+    })
+})
+
