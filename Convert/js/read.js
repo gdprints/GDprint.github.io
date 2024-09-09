0f537240ced585eb78582c5e581a93fcfb7e33f8
GDprints <120603682+GDprint@users.noreply.github.com>
2024-09-09 11:04:23 +0400

    Delete Convert directory

--- Convert/js/read.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".invisible-content").hide();
-    $(document).on('click',"#btn",function(){
-        var moreLessButton=$(".invisible-content").is(":visible")?'Կարդալ ավելին':'Ցույցադրել քիչ';
-        $(this).text(moreLessButton);
-        $(this).parent(".span8").find(".invisible-content").toggle();
-        $(this).parent(".span8").find(".visible-content").toggle();
-    });
-});
