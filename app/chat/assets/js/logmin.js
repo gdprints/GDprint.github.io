d2a20bfc3ea016081ce782a01d5c18efc3864c8f
GDprints <120603682+GDprint@users.noreply.github.com>
2024-10-02 12:20:04 +0400

    Add files via upload

---
+++ app/chat/assets/js/logmin.js
@@ -0,0 +1,19 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Correct credentials
+    const correctUsername = 'Milena';
+    const correctPassword = 'Dinozavr';
+
+    // Get input values
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Check credentials
+    if (username === correctUsername && password === correctPassword) {
+        window.location.href = "hqwertotyuiopmasde.html";  // Redirect to desired page
+    } else {
+        document.getElementById('error-message').innerText = "Incorrect username or password";
+        document.getElementById('error-message').style.display = 'block';
+    }
+});
